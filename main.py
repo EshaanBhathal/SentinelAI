ea76d9c00103128f9b8e179a48f8c512e5d0e53c
bhathale <eshaan.bhathal@mail.utoronto.ca>
2025-08-19 12:55:22 -0400

    reformatted code and added objects for people

--- main.py
+++ main.py
@@ -1,56 +1,66 @@
 import cv2 as cv
-from camera import Camera
 from ultralytics import YOLO
 
+from camera import Camera
+from person import Person
+
 CAMERA_URL = "rtsp://admin:Bhathal@7@10.0.0.252:554/cam/realmonitor?channel=1>&subtype=0"
-# CAMERA_URL = 0    # For testing
+#CAMERA_URL = 0    # For testing
+
 
 
 def main():
     
-    # Creates an object for the camera
+    # Creates objects
     video = Camera(CAMERA_URL, 1920, 1080)
-
     model = YOLO('yolov8n.pt')
-
 
     # loop through frames
     while True:
 
+        # Read the frame
         isCaptured, frame = video.read()
         if not isCaptured:
             break
         video.addText("Press 'q' to Exit", (20,70), 2, (255,255,255), 6)
 
+        # Track objects on the frame
         tracking = model.track(frame, persist=True, conf=0.5)[0]
 
-        # looping through all tracks
+        # looping through all objects
         for box in tracking.boxes:
 
             # Only tracking humans
             if box.id is None or int(box.cls.item()) != 0:
                 break
 
-
-
-            # printing the boxes
             track_id = int(box.id.item()) 
+
+            # New person
+            if track_id not in Person.people_ids:
+                Person(track_id)
+
+            # Finding the person
+            person = Person.people_objects[Person.people_ids.index(track_id)]
+
+            # Updating information for the person
             conf = float(box.conf.item())
             cls = int(box.cls.item())
             box = [int(x) for x in box.xyxy[0]]
-
+            person.setInfo(conf, cls, box)
+            person.increaseFrames()
 
             # Drawing the tracking boxes
-            cv.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
-            cv.putText(frame, f' ID: {track_id}, {conf:.2f}% {model.names[int(cls)]}', (box[0], box[1]-10),cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3, cv.LINE_AA)
+            person.displayBox(frame, model, video.frameWidth, video.frameHeight)
 
-        frame_ = tracking.plot()
-
+        # Display the frame
         cv.imshow('frame', frame)
 
-        if cv.waitKey(20) & 0xFF==ord('q'): # if q pressed, break loop                                                      # Why I need to spam q?
-            video.cleanup()
+        # Exiting the program when 'q' pressed
+        if cv.waitKey(20) & 0xFF==ord('q'):
             break;
+
+    video.cleanup()
 
 
 
---
+++ person.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+class Person:
+
+    people_ids = []
+    people_objects = []
+
+    def __init__(self, track_id):
+        self.track_id = track_id
+        Person.people_ids.append(track_id)
+        Person.people_objects.append(self)
+        self.frames = 1
+
+
+    def __str__(self):
+        return f"Person has track ID: {self.track_id}"
+    
+
+    def setInfo(self, conf, cls, box):
+        self.conf = conf
+        self.cls = cls
+        self.box = box
+
+    def increaseFrames(self):
+        self.frames += 1
+
+
+    def displayBox(self, frame, model, width, height):
+         # Drawing the tracking boxes
+        fontScale = ((self.box[2] - self.box[0]) / width) * 5
+
+        cv.rectangle(frame, (self.box[0], self.box[1]), (self.box[2], self.box[3]), (255, 0, 0), 2)
+        cv.putText(frame, f' ID: {self.track_id}, {self.conf:.2f}% {model.names[int(self.cls)]}', (self.box[0], self.box[1]-10),cv.FONT_HERSHEY_SIMPLEX, fontScale, (255, 0, 0), 3, cv.LINE_AA)
